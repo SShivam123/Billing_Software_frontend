6c8d4593c88314566e483500abdb4e193466458c
SShivam123 <shivampandit7470@gmail.com>
2026-05-24 10:07:02 +0530

    added hash

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom"
 import AppContextProvider from './Pages/AppContextProvider.jsx';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
 
-  <BrowserRouter>
+  // <BrowserRouter>
+  <HashRouter>
     <AppContextProvider>
       <App />
     </AppContextProvider>
-  </BrowserRouter>
+  </HashRouter>
+  // </BrowserRouter>
 
 )
